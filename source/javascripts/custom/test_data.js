f4f8cbc6238c8ecaa6dc033e375aa13b75baa447
Yazin <yazinsai@users.noreply.github.com>
2018-09-28 09:36:42 +0300

    Remove backticks

--- source/javascripts/custom/test_data.js
+++ source/javascripts/custom/test_data.js
@@ -3,12 +3,12 @@
  *   %test_secret_key%
  *   %test_public_key%
  */
-const apiBase = 'https://api.verifypayments.com';
+const API_BASE = 'https://api.verifypayments.com';
 
 $(function(){
   findAndReplaceApiEndpoint();
 
-  fetch(`${apiBase}/test_data`)
+  fetch(API_BASE + "/test_data")
     .then(function(response) {
       return response.json();
     })
@@ -21,7 +21,7 @@ function findAndReplaceApiEndpoint() {
   window.findAndReplaceDOMText(
     document.getElementsByTagName('body')[0], {
       find: /%api_endpoint%/g,
-      replace: apiBase
+      replace: API_BASE
     });
 }
 
